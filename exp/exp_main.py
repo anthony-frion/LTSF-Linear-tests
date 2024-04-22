a58797185e3f1c3eedcc73149f106dfa82b81602
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2024-04-22 10:54:39 +0200

    Update exp_main.py

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -1,6 +1,6 @@
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
-from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear
+from models import Informer, Autoformer, Transformer, DLinear, Linear, NLinear, DLinear-Stochastic, Linear-Stochastic, NLinear-Stochastic
 from utils.tools import EarlyStopping, adjust_learning_rate, visual, test_params_flop
 from utils.metrics import metric
 
