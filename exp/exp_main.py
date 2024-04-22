6c32c6a771fe8bc945b8b123b8e17713fefa2f8f
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2024-04-22 14:29:08 +0200

    Update exp_main.py

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -74,7 +74,7 @@ class Exp_Main(Exp_Basic):
                 if 'Stochastic' in self.args.model:
                     mu, std = self.model(batch_x)
                     f_dim = -1 if self.args.features == 'MS' else 0
-                    mu, = mu[:, -self.args.pred_len:, f_dim:].detach().cpu()
+                    mu = mu[:, -self.args.pred_len:, f_dim:].detach().cpu()
                     std = std[:, -self.args.pred_len:, f_dim:].detach().cpu()
 
                     batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
@@ -157,7 +157,7 @@ class Exp_Main(Exp_Basic):
                 if 'Stochastic' in self.args.model:
                     mu, std = self.model(batch_x)
                     f_dim = -1 if self.args.features == 'MS' else 0
-                    mu, = mu[:, -self.args.pred_len:, f_dim:].detach().cpu()
+                    mu = mu[:, -self.args.pred_len:, f_dim:].detach().cpu()
                     std = std[:, -self.args.pred_len:, f_dim:].detach().cpu()
 
                     batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
@@ -267,7 +267,7 @@ class Exp_Main(Exp_Basic):
                 if 'Stochastic' in self.args.model:
                     mu, std = self.model(batch_x)
                     f_dim = -1 if self.args.features == 'MS' else 0
-                    mu, = mu[:, -self.args.pred_len:, f_dim:].detach().cpu()
+                    mu = mu[:, -self.args.pred_len:, f_dim:].detach().cpu()
                     std = std[:, -self.args.pred_len:, f_dim:].detach().cpu()
 
                     batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
