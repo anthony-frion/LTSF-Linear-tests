51b8bfda3639ca535853ead2a4f24c48dc112f32
Anthony Frion <58110567+anthony-frion@users.noreply.github.com>
2024-04-25 09:38:56 +0200

    Update etth2.sh

--- scripts/EXP-LongForecasting/Linear-Stochastic/etth2.sh
+++ scripts/EXP-LongForecasting/Linear-Stochastic/etth2.sh
@@ -12,55 +12,55 @@ model_name=DLinear_Stochastic
 python -u run_longExp.py \
   --is_training 1 \
   --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'96 \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len'_'96 \
   --model $model_name \
-  --data ETTh1 \
+  --data ETTh2 \
   --features M \
   --seq_len $seq_len \
   --pred_len 96 \
   --enc_in 7 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'96.log
+  --itr 1 --batch_size 32 --learning_rate 0.05 >logs/LongForecasting/$model_name'_'ETTh2_$seq_len'_'96.log
 
 python -u run_longExp.py \
   --is_training 1 \
   --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'192 \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len'_'192 \
   --model $model_name \
-  --data ETTh1 \
+  --data ETTh2 \
   --features M \
   --seq_len $seq_len \
   --pred_len 192 \
   --enc_in 7 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'192.log
+  --itr 1 --batch_size 32 --learning_rate 0.05 >logs/LongForecasting/$model_name'_'ETTh2_$seq_len'_'192.log
 
 python -u run_longExp.py \
   --is_training 1 \
   --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'336 \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len'_'336 \
   --model $model_name \
-  --data ETTh1 \
+  --data ETTh2 \
   --features M \
   --seq_len $seq_len \
   --pred_len 336 \
   --enc_in 7 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'336.log
+  --itr 1 --batch_size 32 --learning_rate 0.05 >logs/LongForecasting/$model_name'_'ETTh2_$seq_len'_'336.log
 
 python -u run_longExp.py \
   --is_training 1 \
   --root_path ./dataset/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_$seq_len'_'720 \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len'_'720 \
   --model $model_name \
-  --data ETTh1 \
+  --data ETTh2 \
   --features M \
   --seq_len $seq_len \
   --pred_len 720 \
   --enc_in 7 \
   --des 'Exp' \
-  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/LongForecasting/$model_name'_'Etth1_$seq_len'_'720.log
+  --itr 1 --batch_size 32 --learning_rate 0.05 >logs/LongForecasting/$model_name'_'ETTh2_$seq_len'_'720.log
